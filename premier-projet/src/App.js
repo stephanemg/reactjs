e0c6547b8fa38ea75e144271914530ba817c26e8
stephanemg <stephane.mg@gmail.com>
2020-05-29 07:56:36 +0200

    i

--- premier-projet/src/App.js
+++ premier-projet/src/App.js
@@ -6,51 +6,78 @@ import Button from "./components/Button";
 const membres = {
   membre1: {
     nom: 'steph',
-    age: 52
+    age: 52,
+    humain:true,
+    attribut: 'je suis le maitre de maison'
   },
   membre2: {
     nom: 'valerie',
-    age: 51
+    age: 51,
+    humain:true,
+    attribut : 'je suis la femme du maitre de maison'
   },
   membre3: {
     nom: 'medor',
-    age: 7
+    age: 7,
+    humain : false,
+    attribut : 'je suis un chien'
   }
 }
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {membres}
-  }
+  state = { membres, isShow: false }
 
   handleClick = ans => {
     const membres = this.state.membres
     membres.membre1.age += ans
     this.setState(membres)
   }
-  handleChange = event => {
+
+  //handleChange est deja cree en fonction fléchée avec l'event du changement de texte qui contient le nom
+  //l'id est le membre en cours d'iteration par map(), donc contient le membre en cours d'edition
+  handleChange = (event, id) => {
     const membres = this.state.membres
     const nom = event.target.value
     console.log(nom)
-    membres.membre1.nom = nom
+    membres[id].nom = nom
     this.setState({ membres })
   }
+
+  cacherNom = id => {
+    const membres = this.state.membres
+    membres[id].nom = 'X'
+    this.setState(membres)
+  }
+
+ 
+
   render() {
     const { titre } = this.props
     const { membres } = this.state
+
+
+    //on constitue un tableau de cles -> objets en partant des membres
+    //on passe au membre la fonction flechee avec le bon parametre du membre en cours
+    //il faut donner une cle unique a chaque element de la liste
+    const liste = Object.keys(membres).map(membre => (
+      <Membre
+        key={membre}
+        handleChange = {event => this.handleChange(event, membre)}
+        age={membres[membre].age}
+        nom={membres[membre].nom}
+        humain={membres[membre].humain}
+        attribut={membres[membre].attribut}
+        cacherNom={() => this.cacherNom(membre)} >
+      </Membre>
+    ))
+//on retourne directement la liste constituée dynamiquement via le traitement précédent.
+//la liste contient pour les composants enfants des appels en fonctions flechées paramétrées
     return (
       <div className="App">
         <h1>{titre}</h1>
-        <input value={membres.membre1.nom} onChange={this.handleChange} type="text" />
-        <Membre membre={membres.membre1} />
-        <Membre membre={membres.membre2} >
-          <strong>
-            je suis ta femme
-           </strong>
-        </Membre>
+        {liste}
         <Button vieillir={() => this.handleClick(2)} />
-      </div>
+      </div >
     );
   }
 }
--- premier-projet/src/components/Membre.js
+++ premier-projet/src/components/Membre.js
@@ -1,13 +1,22 @@
 import React, { Fragment } from 'react'
 
-const Membre = ({ membre, children }) => {
+
+//la fonction cacher nom est declaree au niveau parent par une fonction flechee
+const Membre = ({ age, nom, children, cacherNom , humain, attribut, handleChange}) => {
     return (
         <Fragment>
             <h2 style={{
-                backgroundColor: membre.age > 52 ? 'purple' : 'green',
+                backgroundColor: age > 52 ? 'purple' : 'green',
                 color: 'white'
-            }}>{membre.nom} age : {membre.age}
+            }}>{nom} age : {age}
             </h2>
+            {
+                !humain ?
+                <strong>{attribut}</strong>
+                : null
+            }
+             <input value={nom} onChange={handleChange} type="text" />
+            <button onClick={cacherNom}>X</button><br></br>
             {children ? <p>{children}</p> : <Fragment />}
         </Fragment>
     )
